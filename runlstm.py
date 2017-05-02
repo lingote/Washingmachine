65190430a526fc049da00123fd7f43702fd285ba
ignacio <ignacio@qed.ai>
2017-05-03 00:51:16 +0200

    add option

--- runlstm.py
+++ runlstm.py
@@ -4,7 +4,6 @@ Simple function to run forecast for 1-10 minutes of a given
 SAG mill performance variable
 """
 import argparse
-import timeit
 import SAGMill.SAGMillAnalysis
 import SAGMill.SAGData
 
@@ -28,12 +27,15 @@ def main():
                         choices=perfvars, dest='t', help='target variable')
     parser.add_argument('-s', '--save-forecast', type=bool, required=False,
                         default=False, dest='s', help='save forecast to csv')
-    parser.add_argument('-f', '--fits', type=bool, required=False,
+    parser.add_argument('-f', '--fits', action="store_true", required=False,
                         default=False, dest='f', help='run fits or only prediction')
     parser.add_argument('-m', '--mode', type=str, required=False,
                         default='valid', dest='m', help='train, valid or test mode')
     parser.add_argument('-d', '--draw', type=bool, required=False,
-                        default=False, dest='m', help='draw residuals')
+                        default=False, dest='d', help='draw residuals')
+    parser.add_argument('--model-dir', type=str, required=False,
+                        default='lstm/fitresults/', dest='mdir',
+                        help='location of model files (pkl and h5)')
     args = parser.parse_args()
     sag = SAGMill.SAGData.SAGMillData()
     if args.f:
@@ -44,7 +46,9 @@ def main():
             fits = [i.strip('\n') for i in fits]
     dfyhat = SAGMill.SAGMillAnalysis.tenminuteforecast(sag, target=args.t,
                                                        modellist=fits,
-                                                       mode=args.m, tocsv=args.s)
+                                                       mode=args.m, 
+                                                       tocsv=args.s,
+                                                       modeldir=args.mdir)
     if args.d:
         SAGMill.SAGMillAnalysis.drawresiduals(sag, dfyhat, target=args.t,
                                               mode=args.m, modeldir='lstm')
