db8c3b1ce6efc919fe52d853451e9a70066fad02
ignacio <ignacio@qed.ai>
2017-05-02 18:33:11 +0200

    update example

--- examples.py
+++ examples.py
@@ -1,12 +1,3 @@
-import SAGMill.SAGMillAnalysis
-import pandas as pd
-pd.set_option('display.width',1000)
+# Get 1-10 prediction for PowerDraw
+python runlstm.py -t PowerDrawMW -m train -f
 
-sag = SAGMill.SAGMillAnalysis.SAGMillAnalyzer()
-# run a grid eval on a neural network
-pipeline = sag.nngrideval(offset=3, savemodel=True, logfile='test_basic.log')
-pllist = ['skpipeline_modelPowerDrawMW_3min.pkl']
-# Make prediction. This will return a DataFrame for target variable, one column for each minute
-res = sag.singlevarnnpred(pipelinelist=pllist, target='PowerDrawMW')
-# Calculate resiudals and save plots
-sag.drawsingleresiduals(res, offset=3, mode='valid', target='PowerDrawMW', modeldir='nnet')
